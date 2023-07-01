1f2732ef00c9f956f1d3229b9d66ebfbec195c69
Raphael Machado <raphael.mm.91@gmail.com>
2023-07-01 02:50:53 -0300

    Delete src/util directory

--- src/util/play.ts
+++
@@ -1,4 +0,0 @@
-import {Dispatch,SetStateAction} from "react"
-function Play(minutes: number, seconds:number, setMinutes: Dispatch<SetStateAction<number>>, setSeconds: Dispatch<SetStateAction<number>>){
-    setInterval(()=> ,1000)
-}
